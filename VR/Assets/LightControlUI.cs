384c4a928d3d5fc636f6aadb803ba5c265aa746d
ljch0407 <ljch0407@gmail.com>
2022-08-09 19:27:13 +0900

    라이팅 제어 스크립트

---
+++ VR/Assets/LightControlUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControlUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float curLightIntensity = 0;
+    private Transform trans;
+    public optionLight optionLight;
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        curLightIntensity = trans.rotation.x / 100;
+        optionLight.currentLightIntensity += curLightIntensity;
+    }
+}
