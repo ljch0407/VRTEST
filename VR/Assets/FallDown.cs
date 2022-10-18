4b444ecbff000391ecdbb3ac982064dec5223ef1
Game_engine_Jun <wns0685@gmail.com>
2022-10-18 17:08:54 +0900

    IK 걸음, 타켓 추적 추가 문제 : 기존 monster스크립트랑 문제? pos를 찾지 못함

---
+++ VR/Assets/FallDown.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDown : MonoBehaviour
+{
+    [SerializeField] LayerMask terrainLayer = default;
+    [SerializeField] float delay;
+
+    private Vector3 rayCastStartPos;
+    void Start()
+    {
+        rayCastStartPos = transform.position + new Vector3(0, 30, 0);
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(rayCastStartPos, Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit info, 100, terrainLayer.value))
+        {
+            transform.position = new Vector3(transform.position.x, info.point.y + delay, transform.position.z);
+        }
+    }
+}
---
+++ VR/Assets/LegStepper.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+public class LegStepper : MonoBehaviour
+{
+    [SerializeField] Transform homeTransform;
+    [SerializeField] float wantStepAtDistance;
+    [SerializeField] float moveDuration;
+    [SerializeField] float stepOvershootFraction;
+    [SerializeField] LegStepper otherStepper;
+
+    
+    public bool Moving;
+    private Vector3 newPosition;
+    
+    void Update()
+    {
+        if (Moving || otherStepper.Moving) return;
+
+        float distFromHome = Vector3.Distance(transform.position, homeTransform.position);
+        if (distFromHome > wantStepAtDistance)
+        {
+            StartCoroutine(Move());
+        }
+        
+    }
+    
+    public void TryMove()
+    {
+        if (Moving) return;
+
+        float distFromHome = Vector3.Distance(transform.position, homeTransform.position);
+        if (distFromHome > wantStepAtDistance)
+        {
+            StartCoroutine(Move());
+        }
+    }
+    IEnumerator Move()
+    {
+        Moving = true;
+
+        Vector3 startPoint = transform.position;
+        Quaternion startRot = transform.rotation;
+
+        Quaternion endRot = homeTransform.rotation;
+        Vector3 towardHome = (homeTransform.position - transform.position);
+        float overshootDistance = wantStepAtDistance * stepOvershootFraction;
+        Vector3 overshootVector = towardHome * overshootDistance;
+        overshootVector = Vector3.ProjectOnPlane(overshootVector, Vector3.up);
+
+        Vector3 endPoint = homeTransform.position + overshootVector;
+        Vector3 centerPoint = (startPoint + endPoint) / 2;
+        centerPoint += homeTransform.up * Vector3.Distance(startPoint, endPoint) / 2f;
+
+        float timeElapsed = 0;
+        do
+        {
+            timeElapsed += Time.deltaTime;
+            float normalizedTime = timeElapsed / moveDuration;
+            normalizedTime = Easing.InOutCubic(normalizedTime);
+            transform.position =
+                Vector3.Lerp(
+                    Vector3.Lerp(startPoint, centerPoint, normalizedTime),
+                    Vector3.Lerp(centerPoint, endPoint, normalizedTime),
+                    normalizedTime
+                );
+
+            transform.rotation = Quaternion.Slerp(startRot, endRot, normalizedTime);
+
+            yield return null;
+        }
+        while (timeElapsed < moveDuration);
+
+        Moving = false;
+    }
+    
+    
+}
