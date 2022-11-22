3f3d476a52e584fdfa71675c37243eb00a761221
ljch0407 <ljch0407@gmail.com>
2022-11-22 09:56:17 +0900

    바리케이드 효과음 적용중, 기본 플레이 세팅

--- VR/Assets/Scripts/Barricade.cs
+++ VR/Assets/Scripts/Barricade.cs
@@ -7,15 +7,17 @@ public class Barricade : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody rigidBody;
+    public SoundManager _soundManager;
+
     void Start()
     {
-        
+        _soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>().instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,6 +25,13 @@ public class Barricade : MonoBehaviour
         if (other.tag == "Hammer")
         {
             rigidBody.isKinematic = false;
+           // StartCoroutine(BreakBarricades());
         }
+    }
+
+    IEnumerator BreakBarricades()
+    {
+        _soundManager.PlaySFX("BreakWoodenBarricades");
+        yield return new WaitForSeconds(0.1f);
     }
 }
