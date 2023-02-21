8c9d39096cf823c848ab2725f4d3f372501ea40b
ljch0407 <ljch0407@gmail.com>
2023-02-21 21:49:47 +0900

    스테이지2 프리펩에 전등 추가

--- VR/Assets/Scripts/StatueCheckBox.cs
+++ VR/Assets/Scripts/StatueCheckBox.cs
@@ -1,12 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class StatueCheckBox : MonoBehaviour
 {
     public GameObject Statue;
-    
+    private bool StatueAssembled;
+
+    private void Awake()
+    {
+        StatueAssembled = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position.y < -300)
+            StatueAssembled = false;
+        
+        if (StatueAssembled)
+        {
+            transform.position += Vector3.down * Time.deltaTime * 1;
+        }
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
@@ -16,6 +35,7 @@ public class StatueCheckBox : MonoBehaviour
             if (playerInfo.upperStatue && playerInfo.midStatue && playerInfo.underStatue)
             {
                 Statue.SetActive(true);
+                StatueAssembled = true;
             }
         }
 
