fbc8abaefd7f203ddf841ffdddf21815f3b58af2
Game_engine_Jun <wns0685@gmail.com>
2023-02-27 01:23:15 +0900

    no message

--- VR/Assets/Scripts/RedFog.cs
+++ VR/Assets/Scripts/RedFog.cs
@@ -2,34 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum RedFogState
+{
+    Mid,
+    Under
+    
+}
+
 public class RedFog : MonoBehaviour
 {
     public PlayerInfo PlayerInfo;
 
-    public bool FirstFog;
-    
-    void Start()
-    {
-        
-    }
+    public bool FogActivate = true;
+    public RedFogState state;
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        if (FirstFog)
+        if (FogActivate)
         {
-            if (PlayerInfo.underStatue)
+            if (PlayerInfo.underStatue && state == RedFogState.Under)
+            {
+                gameObject.SetActive(false);
+            }
+            else if (PlayerInfo.midStatue && state == RedFogState.Mid)
             {
                 gameObject.SetActive(false);
             }
         }
-        else
-        {
-            if (PlayerInfo.midStatue)
-            {
-                gameObject.SetActive(false);
-            }
-        }
-
     }
 }
